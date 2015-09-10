0ef7dba46c5c90356f2025572cf1a668eb9e3dc6
Johannes Rudolph <johannes.rudolph@gmail.com>
2015-09-10 12:23:05 +0200

    simple spray.io streaming log server

--- project/ProjectBuild.scala
+++ project/ProjectBuild.scala
@@ -10,7 +10,7 @@ import spray.revolver.RevolverPlugin._
 object ProjectBuild extends Build {
   lazy val root =
     Project("root", file("."))
-      .aggregate(frontend, backend)
+      .aggregate(frontend, backend, logService)
 
   // Scala-Js frontend
   lazy val frontend =
@@ -41,6 +41,16 @@ object ProjectBuild extends Build {
           (fastOptJS in Compile in frontend, packageScalaJSLauncher in Compile in frontend)
             .map((f1, f2) => Seq(f1.data, f2.data)),
         watchSources <++= (watchSources in frontend)
+      )
+
+  lazy val logService =
+    Project("log-service", file("log-service"))
+      .settings(Revolver.settings: _*)
+      .settings(commonSettings: _*)
+      .settings(
+        libraryDependencies ++= Seq(
+          "com.typesafe.akka" %% "akka-http-experimental" % "1.0"
+        )
       )
 
   def commonSettings = Seq(
